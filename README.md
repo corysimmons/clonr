75c69075e4ec9b40438367387ec5466541d1c0cd
Cory Simmons <cory@mojotech.com>
2013-11-06 13:56:39 -0500

    Create proj

---
+++ README.md
@@ -0,0 +1,10 @@
+clonr
+---
+
+Clone repos from Github quickly and easily without having to type all the Github cruft each time.
+
+###Installation
+- `npm install -g clonr`
+
+###Usage
+- `clonr corysimmons/jeet-html` or `clonr corysimmons/jeet-html foo`
---
+++ clonr.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var exec = require('child_process').exec
+    , p = process.argv;
+
+if(p[3]) {
+    console.log('git clone https://github.com/'+ p[2] +'.git '+ p[3]);
+    exec('git clone https://github.com/'+ p[2] +'.git '+ p[3], function(error, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        if (error !== null) {
+            console.log(error);
+        }
+    });
+} else {
+    console.log('git clone https://github.com/'+ p[2] +'.git');
+    exec('git clone https://github.com/'+ p[2] +'.git', function(error, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        if (error !== null) {
+            console.log(error);
+        }
+    });
+}
